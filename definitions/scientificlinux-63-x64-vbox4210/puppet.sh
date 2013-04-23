7421b60c7c64ebb92b8132b47b2ddcdb82bd6c04
Charlie Sharpsteen <chuck@puppetlabs.com>
2013-04-23 11:39:41 -0700

    Scientific Linux: Don't install Puppet during kick
    Puppet installation should be handled post-kick by `puppet.sh`.

--- definitions/scientificlinux-63-x64-vbox4210/puppet.sh
+++ definitions/scientificlinux-63-x64-vbox4210/puppet.sh
@@ -1,1 +1,12 @@
-gem install --no-ri --no-rdoc puppet
+# Install Puppet
+
+cat > /etc/yum.repos.d/puppetlabs.repo << EOM
+[puppetlabs]
+name=puppetlabs
+baseurl=http://yum.puppetlabs.com/el/6/products/\$basearch
+enabled=1
+gpgcheck=0
+EOM
+
+yum -y install puppet facter
+
